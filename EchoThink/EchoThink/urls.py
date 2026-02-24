4a64c7e846411c760f6b4b24fb168ac5f7a9f750
Francisco Fernandes do Vale Segundo <ffvsegundo@gmail.com>
2026-02-24 15:03:19 -0300

    fix media

--- EchoThink/EchoThink/urls.py
+++ EchoThink/EchoThink/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from .views import CSRFTokenView, CurrentUserView
 
 urlpatterns = [
@@ -28,4 +29,11 @@ urlpatterns = [
     path("api/csrf/", CSRFTokenView.as_view(), name="get_csrf"),
     path("me/", CurrentUserView.as_view()),
     path('ValidateTokenView/', views.ValidateTokenView.as_view(), name='validate_token'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
